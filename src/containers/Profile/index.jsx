70a76ad1b04c30c5bc3eafdf870637f650cc3ad6
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2024-01-27 22:48:59 +0500

    upgrade commit

--- src/containers/Profile/index.jsx
+++ src/containers/Profile/index.jsx
@@ -1,37 +1,49 @@
 import React from "react";
-import './style.scss'
+import "./style.scss";
 import { useNavigate } from "react-router-dom";
-import { Animate } from 'react-simple-animate'
+import { Animate } from "react-simple-animate";
 
 const Profile = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
-  const navigateToContactHandler = ()=> {
-    navigate("/contact")
-  }
+  const navigateToContactHandler = () => {
+    navigate("/contact");
+  };
   return (
     <section id="profile" className="Profile">
       <div className="Profile_text-wrap">
         <h1>
           Assalam-o-Alaikum,
-          <br/>
-          I am Muhammad Jawwad Khan
-          
+          <br />I am Muhammad Jawwad Khan
           {/* Front-End Developer */}
         </h1>
+      <Animate
+        play
+        duration={3}
+        delay={0}
+        start={{ transform: "translatex(-1550px)" }}
+        end={{ transform: "translatex(0px)" }}
+      >
+
         <p>
-          Tech enthusiast | BSCS student at University of Karachi | 
-          <br/>Proficient in Java, OOP, Python, HTML, CSS and JavaScript (React Js) | 
-          <br/>Creative problem solver | Lifelong learner
+          Tech enthusiast | BSCS student at University of Karachi |
+          Proficient in Java, OOP, Python, HTML, CSS and JavaScript (React Js) |
+          Creative problem solver | Lifelong learner
         </p>
+        </Animate>
       </div>
 
-      <Animate play duration={1.5} delay={1} start={{transform: 'translateY(550px)',}} end={{transform: 'translatex(0px)',}}>
-      <div className="Profile_button">
-        <button onClick={navigateToContactHandler}>Hire Me</button>
+      <Animate
+        play
+        duration={1.5}
+        delay={1}
+        start={{ transform: "translateY(550px)" }}
+        end={{ transform: "translatex(0px)" }}
+      >
+        <div className="Profile_button">
+          <button onClick={navigateToContactHandler}>Hire Me</button>
         </div>
       </Animate>
-      
     </section>
   );
 };
