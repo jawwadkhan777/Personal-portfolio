63fe75ccb768c8d57266608dd82b59fa56a9bb86
Muhammad Jawwad Khan <m.jawwadkhan777@gmail.com>
2024-01-30 18:14:09 +0500

    upgrade commit

--- src/containers/Contact/index.jsx
+++ src/containers/Contact/index.jsx
@@ -4,64 +4,27 @@ import { BsFillChatSquareTextFill } from "react-icons/bs";
 import "./style.scss";
 import { Animate } from "react-simple-animate";
 
-
 const Contact = () => {
-
-  // const [name, setName] = useState('');
-  // const [isValidName, setIsValidName] = useState(true)
-  // const [email, setEmail] = useState('');
-  // const [message, setMessage] = useState('');
   const [details, setDetails] = useState({
-    name: '',
-    email: '',
-    message: ''
-  })
+    name: "",
+    email: "",
+    message: "",
+  });
 
-  const onChangeHandler = (event)=> {
-    const {id, value} = event.target
-    setDetails((prev)=> {return{...prev, [id]: value}})
-  }
-  // console.log(details)
+  const onChangeHandler = (event) => {
+    const { id, value } = event.target;
+    setDetails((prev) => {
+      return { ...prev, [id]: value };
+    });
+  };
 
-  const onClickHandler = ()=> {
+  const onSubmitHandler = () => {
     // console.log(details)
-    alert(`Thanks for filling the form!!! Your name: ${details.name} Your email: ${details.email} your message: ${details.message}`)
-  }
-
-  // useEffect(()=> {
-  //   popUpHandler() 
-  // }, [])
-
-  // const popUpHandler = ()=> {
-  //   alert("Thanks for filling.")
-  // }
-
-  // const inputNameHandler = (event)=> {
-    // setInput(input)
-    // setName(event.target.value)
-    // setIsValidName(event.target.value !== '')    
-    // setEmail(event.target.value)    
-    // setMessage(event.target.value)    
-  // }
-  // const inputEmailHandler = (event)=> {
-    // setInput(input)
-    // setName(event.target.value)    
-    // setEmail(event.target.value)    
-    // setMessage(event.target.value)    
-  // }
-  // const inputMessageHandler = (event)=> {
-    // setInput(input)
-    // setName(event.target.value)    
-    // setEmail(event.target.value)    
-    // setMessage(event.target.value)    
-  // }
-
-
-    // console.log(name)
-    // console.log(email)
-    // console.log(message)
-
-
+    alert(
+      `Thanks for filling the form!!! Your name: ${details.name} Your email: ${details.email} your message: ${details.message}`
+    );
+    setDetails(" ");
+  };
 
   return (
     <section id="contact" className="Contact">
@@ -87,21 +50,46 @@ const Contact = () => {
           end={{ transform: "translatex(0px)" }}
         >
           <div className="Contact_container_formWrap">
-            <form className="Contact_container_formWrap_inputs">
+            <form className="Contact_container_formWrap_inputs" onSubmit={onSubmitHandler}>
               <div>
-                <input required id="name" type={"text"} className="nameInput" onChange={onChangeHandler}></input>
-                <label htmlFor="name" className="nameLable">Name</label>
+                <input
+                  required
+                  id="name"
+                  type={"text"}
+                  className="nameInput"
+                  onChange={onChangeHandler}
+                ></input>
+                <label htmlFor="name" className="nameLable">
+                  Name
+                </label>
               </div>
               <div>
-                <input required id="email" type={"text"} className="emailInput" onChange={onChangeHandler}></input>
-                <label htmlFor="email" className="emailLable">Email</label>
+                <input
+                  required
+                  id="email"
+                  type={"text"}
+                  className="emailInput"
+                  onChange={onChangeHandler}
+                ></input>
+                <label htmlFor="email" className="emailLable">
+                  Email
+                </label>
               </div>
               <div>
-                <textarea required rows={7} id="message" type={"text"} className="messageInput" onChange={onChangeHandler}></textarea>
-                <label htmlFor="message" className="messageLable">Message</label>
+                <textarea
+                  required
+                  rows={7}
+                  id="message"
+                  type={"text"}
+                  className="messageInput"
+                  onChange={onChangeHandler}
+                ></textarea>
+                <label htmlFor="message" className="messageLable">
+                  Message
+                </label>
               </div>
+              <button>Submit</button>
             </form>
-            <button onClick={onClickHandler}>Submit</button>
           </div>
         </Animate>
       </div>
